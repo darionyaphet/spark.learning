dec5af6544c6e82b4c7e60cd9ef40464fbe9b33d
darionyaphet <darion.yaphet@gmail.com>
2016-01-20 19:06:21 +0800

    daily publish

--- src/main/java/org/darion/yaphet/spark/streaming/Main.java
+++ src/main/java/org/darion/yaphet/spark/streaming/Main.java
@@ -1,5 +1,13 @@
 package org.darion.yaphet.spark.streaming;
 
-public class Main {
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.*;
 
+public class Main {
+	public static void main(String[] args) {
+		SparkConf conf = new SparkConf().setAppName("").setMaster("");
+		JavaStreamingContext context = new JavaStreamingContext(conf, new Duration(1000));
+		
+	}
 }
